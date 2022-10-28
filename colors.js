bd0b0f6ae4fd4c11b95e7cfdfca89fc4e42f2618
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-10-28 00:10:08 -0300

    feat: :sparkles: Inicio da persolização
    Inicio da persolização

---
+++ colors.js
@@ -0,0 +1,44 @@
+export const THEME = {
+    COLORS: {
+        BACKGROUND_1: '#171717',
+        BACKGROUND_2: '#141414',
+
+        TEXT: '#FFFFFF',
+
+        PRIMARY: '#C15147',
+        PRIMARY_SOFT: '#C15147' + '50',
+        NEUTRAL_1: '#FFFFFF',
+        NEUTRAL_2: '#969696',
+        NEUTRAL_3: '#464646',
+        NEUTRAL_4: '#2A2A2A',
+        SEMANTIC_1: '#FFC107',
+        SEMANTIC_2: '#6C6A6A',
+        SEMANTIC_3: '#F9F8FD',
+
+        BLACK: '#000000',
+        SUCCESS: '#28A745',
+        WARNING: '#FFC107',
+        DANGER: '#DC3545',
+        INFO: '#17A2B8',
+    },
+    FONT_FAMILY: {
+        REGULAR: 'Inter_400Regular',
+        SEMI_BOLD: 'Inter_600SemiBold',
+        BOLD: 'Inter_700Bold',
+        BLACK: 'Inter_900Black',
+        DANCINGSCRIPT: 'DancingScript_400Regular',
+        DANCINGSCRIPT_BOLD: 'DancingScript_600SemiBold'
+    },
+    FONT_SIZE: {
+        DT: 10,
+        SM: 14,
+        MD: 16,
+        LG: 24,
+        XL: 32,
+        XXL: 48,
+    },
+    SPACING: {
+        MARGIN: 16,
+        PADDING: 12
+    }
+}
